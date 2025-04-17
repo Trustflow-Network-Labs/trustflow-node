f01de724580f8c1d52897d6b986af837f5b30b23
adgsm <momcilo@dzunic.net>
2025-04-17 09:33:23 +0400

    check and install dependencies

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/adgsm/trustflow-node/cmd"
+	"github.com/adgsm/trustflow-node/dependencies"
 )
 
 func main() {
+	dependencies.CheckAndInstallDependencies()
+	fmt.Println("\n🚀 Dependencies checked. Continuing to start the app...")
+
 	cmd.Execute()
 }
